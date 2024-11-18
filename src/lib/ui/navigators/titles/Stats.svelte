e99e6f84ba0400a1b19f0de5f534bebadb804d25
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-18 15:44:31 +0000

    dark mode for stats

--- src/lib/ui/navigators/titles/Stats.svelte
+++ src/lib/ui/navigators/titles/Stats.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="grid grid-cols-2">
-  <div class="bg-gray-100 p-1 text-xs">Courses: {presenceService.coursesOnline.value.length}</div>
-  <div class="bg-gray-100 p-1 text-xs">Students: {presenceService.studentsOnline.value.length}</div>
-  <div class="bg-gray-100 p-1 text-xs">Total: {catalogueCount.value}</div>
-  <div class="bg-gray-100 p-1 text-xs">Total: {studentCount.value}</div>
+  <div class="bg-gray-100 dark:bg-gray-800 p-1 text-xs">Courses: {presenceService.coursesOnline.value.length}</div>
+  <div class="bg-gray-100 dark:bg-gray-800 p-1 text-xs">Students: {presenceService.studentsOnline.value.length}</div>
+  <div class="bg-gray-100 dark:bg-gray-800 p-1 text-xs">Total: {catalogueCount.value}</div>
+  <div class="bg-gray-100 dark:bg-gray-800 p-1 text-xs">Total: {studentCount.value}</div>
 </div>
