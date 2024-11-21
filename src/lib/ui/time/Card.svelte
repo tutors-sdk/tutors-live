5da0027b7acae9ac09551b0797b39b7d646b0275
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-21 08:55:48 +0000

    card tidy up

--- src/lib/ui/time/Card.svelte
+++ src/lib/ui/time/Card.svelte
@@ -1,44 +1,18 @@
 <script lang="ts">
-  // import { Avatar } from "@skeletonlabs/skeleton-svelte";
   import Iconify from "@iconify/svelte";
   import Icon from "$lib/ui/themes/icons/Icon.svelte";
   import { avatarWidth, cardWidths, headingText, iconHeight, imageWidth, textSize } from "$lib/runes";
   import type { CardDetails } from "$lib/services/presence-types.svelte";
-  import { Avatar } from "@skeletonlabs/skeleton-svelte";
   import { getTypeColour } from "../themes/styles/icon-lib.svelte";
 
   export let cardDetails: CardDetails;
 </script>
 
-<!-- <a href={cardDetails.route} target="_blank" rel="noreferrer">
-  <div
-    class="card bg-surface-50 border-[1px] border-y-8 border-primary-500 card-hover divide-surface-200-800 block divide-y overflow-hidden m-2 flex flex-col {cardWidths.value} bg-gray-100 dark:bg-gray-800"
-  >
-    <header class="h-[200px] flex items-center justify-center">
-      {#if cardDetails.icon}
-        <Iconify icon={cardDetails.icon.type} color={cardDetails.icon.color} height={iconHeight.value} />
-      {:else}
-        <img src={cardDetails.img} class="w-12 w-full hue-rotate-90 object-scale-down h-full" alt="banner" />
-      {/if}
-    </header>
-
-    <article class="space-y-4 p-4 flex-grow text-center">
-      <div>
-        <h1 class={textSize.value}>{cardDetails.title}</h1>
-      </div>
-    </article>
-
-    <footer class="p-4 text-center">
-      <small class="opacity-60">{cardDetails.subtitle1}</small>
-    </footer>
-  </div>
-</a> -->
-
 <a href={cardDetails.route} target="_blank" rel="noreferrer">
   <div
     class="card preset-filled-{getTypeColour(cardDetails.type)}-50-950 border-[1px] border-y-8 border-{getTypeColour(
       cardDetails.type
-    )}-500 m-2 {cardWidths.value} transition-all hover:scale-105"
+    )}-500 m-2 {cardWidths.value} transition-all hover:scale-105 h-[320px] flex flex-col"
   >
     <div class="flex">
       <header class="card-header flex flex-row items-center justify-between p-3">
@@ -53,7 +27,7 @@
         <div class="flex-none"><Icon type={cardDetails.type} height="30" /></div>
       </header>
     </div>
-    <div class="card-body">
+    <div class="card-body flex-1">
       <figure class="flex justify-center object-scale-down p-1">
         {#if cardDetails.icon}
           <Iconify icon={cardDetails.icon.type} color={cardDetails.icon.color} height={iconHeight.value} />
@@ -63,8 +37,8 @@
       </figure>
     </div>
     <footer class="card-footer">
-      <div class="-m-4 mt-2 text-center">
-        <div class="inline-flex w-full">
+      <div class="text-center pb-2">
+        <div class="inline-flex w-full items-end justify-center">
           <div class="{textSize.value} flex-auto font-semibold line-clamp-1">
             {cardDetails.subtitle1}
           </div>
