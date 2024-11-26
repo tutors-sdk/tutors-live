65206bd4f202da9e902c86d1f5c2dc1b599cf91f
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-26 11:51:14 +0000

    restructure + introduce tooltips

--- src/lib/ui/navigators/NavButton.svelte
+++ src/lib/ui/navigators/NavButton.svelte
@@ -5,9 +5,10 @@
   export let text: string;
   export let href: string;
   export let isActive: boolean = false;
+  export let tip: string = "";
 </script>
 
 <a {href} class="flex flex-col items-center hover:scale-110 transition-transform p-1 rounded-xl {isActive ? 'text-primary scale-110 bg-gray-200 dark:bg-gray-600' : ''}">
-  <Icon type={iconType} height="30" />
+  <Icon type={iconType} height="30" {tip} />
   <span class="text-sm text-bold">{text}</span>
 </a>
