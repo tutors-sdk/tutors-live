b1953ed41ce873077eb4a48bf37153c2f5ada0e5
Eamonn de Leastar <edeleastar@wit.ie>
2024-12-20 18:50:54 +0000

    remove preview modules from catalogue

--- src/routes/catalogue/+page.ts
+++ src/routes/catalogue/+page.ts
@@ -2,7 +2,9 @@ import { getCatalogue } from "$lib/services/catalogue";
 
 export const load = async ({ params, parent, fetch }) => {
   let catalogueEntries = await getCatalogue();
-  catalogueEntries = catalogueEntries.sort((a, b) => b.visit_count - a.visit_count);
+  catalogueEntries = catalogueEntries
+    .filter(entry => entry.visit_count >= 4)
+    .sort((a, b) => b.visit_count - a.visit_count);
   const courseRecords = catalogueEntries.map((entry) => entry.course_record);
   return {
     courseRecords
